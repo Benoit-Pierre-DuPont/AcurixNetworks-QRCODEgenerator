5f5bd1898a6fd9a32e5f2fd0671fd71bce91537d
Benoit-Pierre-DuPont <jack.r.atkin@gmail.com>
2024-07-29 10:47:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request, send_file
 import qrcode
-from PIL import Image
-import os
 import io
 
 app = Flask(__name__)
 
-def generate_qr_with_logo(data, logo_path):
+def generate_qr(data):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -16,24 +14,7 @@ def generate_qr_with_logo(data, logo_path):
     qr.add_data(data)
     qr.make(fit=True)
 
-    qr_img = qr.make_image(fill='black', back_color='white').convert('RGB')
-
-    try:
-        logo = Image.open(logo_path)
-    except FileNotFoundError:
-        return None
-
-    qr_width, qr_height = qr_img.size
-    logo_size = qr_width // 8
-    logo = logo.resize((logo_size, logo_size), Image.Resampling.LANCZOS)
-
-    white_box_size = (logo_size + 20, logo_size + 20)
-    white_box = Image.new("RGBA", white_box_size, "white")
-    white_box.paste(logo, (10, 10), logo.convert("RGBA"))
-
-    logo_position = ((qr_width - white_box_size[0]) // 2, (qr_height - white_box_size[1]) // 2)
-    qr_img = qr_img.convert("RGBA")
-    qr_img.paste(white_box, logo_position, white_box)
+    qr_img = qr.make_image(fill='black', back_color='white')
 
     img_byte_arr = io.BytesIO()
     qr_img.save(img_byte_arr, format='PNG')
@@ -49,13 +30,12 @@ def index():
         encryption = request.form['encryption']
 
         data = f"WIFI:T:{encryption};S:{ssid};P:{password};;"
-        logo_path = os.path.join(app.root_path, 'static', 'logo.png')
 
-        img_byte_arr = generate_qr_with_logo(data, logo_path)
+        img_byte_arr = generate_qr(data)
         if img_byte_arr:
             return send_file(img_byte_arr, mimetype='image/png', as_attachment=True, download_name=f"{ssid}.png")
         else:
-            return "Error generating QR code. Logo file not found.", 400
+            return "Error generating QR code.", 400
 
     return render_template('index.html')
 
